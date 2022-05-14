810e5736f529e72b392158be9ce55ee9b0e929fe
Daniel Rotband <rotbandnunes@hotmail.com>
2022-05-14 17:41:08 -0300

    ajustes

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class AlunoForm {
 
   @NotEmpty(message = "Preencha o campo corretamente.")
-  @Size(min = 3, max =50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
+  @Size(min = 3, max =50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres!")
   private String nome;
 
   @NotEmpty(message = "Preencha o campo corretamente.")
@@ -22,7 +22,7 @@ public class AlunoForm {
   private String cpf;
 
   @NotEmpty(message = "Preencha o campo corretamente.")
-  @Size(min = 3, max =50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
+  @Size(min = 3, max =50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres!")
   private String bairro;
 
   @NotNull(message = "Prencha o campo corretamente.")
